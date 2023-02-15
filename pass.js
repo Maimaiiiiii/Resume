5ec62bfb781caf5388a8f10994230e6628279ec7
Maimaiiiiii <123919752+Maimaiiiiii@users.noreply.github.com>
2023-02-15 14:21:29 +0800

    Delete pass.js

--- pass.js
+++
@@ -1,8 +0,0 @@
-function myPassword (){
-
-    var password= document.getElementById("password").value;
-
-    if( password.length < 8){
-        alert("Password must be at least 8 characters.")
-    }
-  }
